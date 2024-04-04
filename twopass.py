8b7f90ec9180cf076fe03a839140c1f172894ce1
turner <douglass.turner@gmail.com>
2024-04-04 19:37:01 -0400

    twopass.py build out

--- twopass.py
+++ twopass.py
@@ -64,15 +64,53 @@ def initializeHeader(spacewalkFile, spacewalkMetaData):
 
     return hash
 
-dictionary = {}
+def create_region_list(file):
 
-def build_region_list(file):
+    hash = {}
     for line in file:
         tokens = line.split()
         if 6 == len(tokens):
             key = tokens[0] + '%' + tokens[1] + '%' + tokens[2]
-            if key not in dictionary.keys():
-                dictionary[key] = [tokens[0], int(tokens[1]), int(tokens[2])]
+            if key not in hash.keys():
+                hash[key] = [tokens[0], int(tokens[1]), int(tokens[2])]
+
+    # Build sorted region list
+    result = list(map(lambda string: string.split('%'), list(hash.keys())))
+
+    return result
+
+region_dictionary = {}
+indices = []
+
+def create_spatial_positon_datasets(file):
+    spatial_position_group = root.create_group('spatial_position')
+    xyz_dictionary = None
+
+    for line in file:
+        tokens = line.split()
+        if 'trace' == tokens[0]:
+            indices.append(int(tokens[1]))
+            _string = 'trace(' + tokens[1] + ')'
+            print(_string)
+            if xyz_dictionary is not None:
+                trace_group_name = str(indices[-1] - 1)
+                trace_group = spatial_position_group.create_group(trace_group_name)
+                for key in xyz_dictionary.keys():
+                    xyz = xyz_dictionary[key]
+                    xyz_stack = np.column_stack((xyz[0], xyz[1], xyz[2]))
+                    _string = str(region_dictionary[key])
+                    trace_group.create_dataset(_string, data=xyz_stack)
+            xyz_dictionary = {}
+        elif 6 == len(tokens):
+            key = '%'.join([tokens[0], tokens[1], tokens[2]])
+            if key not in xyz_dictionary.keys():
+                xyz_dictionary[key] = [[], [], []]
+            else:
+                xyz_dictionary[key][0].append(to_float(tokens[3]))
+                xyz_dictionary[key][1].append(to_float(tokens[4]))
+                xyz_dictionary[key][2].append(to_float(tokens[5]))
+
+    return xyz_dictionary
 
 name = arguments.arg_name
 cndbf = h5py.File(name + '.cndb', 'w')
@@ -91,13 +129,25 @@ root = cndbf.create_group(spacewalkMetaData['name'])
 frame = []
 root.create_dataset('time', data=np.array(frame))
 
-build_region_list(spacewalkFile)
-
-region_list = list(map(lambda string: string.split('%'), list(dictionary.keys())))
-
-# create genomic position dataset consisting of the list of regions
+# First pass. Create genomic position dataset consisting of the list of regions
+region_list = create_region_list(spacewalkFile)
 genomicPosition = root.create_group('genomic_position')
 genomicPosition.create_dataset('regions', data=region_list)
+
+# Build region dictionary
+for region in region_list:
+    key = region[0] + '%' + region[1] + '%' + region[2]
+    region_dictionary[key] = region_list.index(region)
+
+# Second pass. Build spatial_position datasets
+spacewalkFile.seek(0)
+
+# discard: ##format=sw1 name=IMR90 genome=hg38
+spacewalkFile.readline()
+# discard: chromosome	start	end	x	y	z
+spacewalkFile.readline()
+
+result = create_spatial_positon_datasets(spacewalkFile)
 
 cndbf.close()
 
