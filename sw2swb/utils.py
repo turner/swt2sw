33b4de1fe5c05471a5c9dd089dc838ab56b49a3a
Douglass Turner <douglass.turner@gmail.com>
2024-05-03 17:40:07 -0400

    Revise multi-point dataset design (#9)

--- sw2swb/utils.py
+++
@@ -1,5 +0,0 @@
-def to_float(value):
-    try:
-        return float(value)
-    except ValueError:  # This will catch cases where the conversion fails, e.g., if value is 'nan'
-        return float(0)
